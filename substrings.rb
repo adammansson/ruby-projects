6c7e138c4683a2287f4c1af07abdfaf9594093b4
Adam Månsson <adam.s.mansson@gmail.com>
2022-06-21 14:40:39 +0200

    Add substrings project

---
+++ substrings.rb
@@ -0,0 +1,15 @@
+def substrings(sentence, dictionary)
+  sentence.split.reduce(Hash.new(0)) do |accumulator, value|
+    dictionary.each do |word|
+      if value.include?(word)
+        accumulator[word] += 1
+      end
+    end
+    accumulator
+  end
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
+
