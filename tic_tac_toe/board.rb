2ee8b3a7160581b3cbe3c08171e5d7bad87f6a4e
Adam Månsson <adam.s.mansson@gmail.com>
2022-06-22 15:29:59 +0200

    Add tic tac toe project

---
+++ tic_tac_toe/board.rb
@@ -0,0 +1,45 @@
+class Board
+  def initialize()
+    @matrix = Array.new(3) { Array.new(3) }
+  end
+
+  def make_move(move, marker)
+    unless @matrix[move[0]][move[1]]
+      @matrix[move[0]][move[1]] = marker
+    else
+      raise "That square is occupied"
+    end
+  end
+
+  def is_won?
+    @matrix.each do |row|
+      return true if row.first && row.all? { |sq| sq == row.first }
+    end
+    @matrix.transpose.each do |col|
+      return true if col.first && col.all? { |sq| sq == col.first }
+    end
+    first_diagonal = (0..2).map { |i| @matrix[i][i] }
+    if first_diagonal.first
+      return true if first_diagonal.all? { |sq| sq == first_diagonal.first }
+    end
+    second_diagonal = [@matrix[0][2], @matrix[1][1], @matrix[2][0]]
+    if second_diagonal.first
+      second_diagonal.all? { |sq| sq == second_diagonal.first }
+    end
+    false
+  end
+
+  def display
+    @matrix.each_with_index do |row, row_index|
+      print "| "
+      row.each_with_index do |marker, col_index|
+        if marker
+          print marker + " | "
+        else
+          print "  | "
+        end
+      end
+      print "\n"
+    end
+  end
+end
---
+++ tic_tac_toe/game.rb
@@ -0,0 +1,65 @@
+require "./player.rb"
+require "./board.rb"
+
+class Game
+  @@MARKERS = ["X", "O"]
+  def Game.get_players()
+    players = {}
+    for i in 0..1 do
+      print "Enter name: "
+      name = gets.chomp
+      marker = @@MARKERS[i]
+      players[marker] = Player.new(name, marker)
+      puts "Player #{name} added with marker #{marker}"
+    end
+    players
+  end
+
+  def Game.get_move()
+    puts "Enter move (two integers with a space between): "
+    begin
+      move = gets.chomp.split
+      raise "Wrong number of arguments" unless move.length == 2
+      raise "Arguments are too long" unless move.all? { |str| str.length == 1 }
+      raise "Arguments are not integers" unless move.all? { |str| str.ord.between?(48, 57) }
+      move = move.map { |str| str.to_i }
+      return move
+    rescue StandardError=>e
+      puts "#{e}, try again..."
+      retry
+    end
+  end
+
+  def initialize()
+    @players = Game.get_players()
+    @board = Board.new
+    @winner = nil
+  end
+
+  def playRound(current_turn)
+    begin
+      move = Game.get_move()
+      @board.make_move(move, current_turn)
+      puts "#{current_turn} was placed at #{move}"
+    rescue StandardError=>e
+      puts "#{e}, try again..."
+      retry
+    end
+    @board.display
+    if @board.is_won?
+      puts "#{@players[current_turn].name} is the winner!"
+      @winner = @players[current_turn]
+    end
+  end
+
+  def playGame
+    turn_counter = 0
+    until @winner do
+      playRound(@@MARKERS[turn_counter % 2])
+      turn_counter += 1
+    end
+  end
+end
+
+my_game = Game.new
+my_game.playGame
